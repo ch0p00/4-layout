9d3fb163ef56ec6e6e1c1a71895e86d7ef0dbef8
ch0p00 <131778128+ch0p00@users.noreply.github.com>
2023-05-11 09:15:11 +0300

    Update script.js
    Some bug fixes

--- js/script.js
+++ js/script.js
@@ -1,15 +1,14 @@
 const burgerMenu = document.querySelector('.burger-menu');
 const menu = document.querySelector('.menu');
-const container = document.querySelector('.header__container')
+const headerContainer = document.querySelector('.header__container');
 
 burgerMenu.addEventListener('click', () => {
     burgerMenu.classList.toggle('open');
     menu.classList.toggle('open');
-    container.classList.toggle('open')
+    headerContainer.classList.toggle('open');
 });
-
-// Hide the menu when a link is clicked
 menu.addEventListener('click', () => {
     burgerMenu.classList.remove('open');
     menu.classList.remove('open');
+    headerContainer.classList.remove('open');
 });
